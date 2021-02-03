3be5addcbd7425d16a5cb5a1344f6deb0fdddae4
cassidydpeterson <cassidydpeterson@gmail.com>
2021-02-03 17:46:04 -0500

    Update MSE_Master for annual OM updating

--- MSE_Master.R
+++ MSE_Master.R
@@ -48,7 +48,7 @@
 # MSE FUNCTION:
 #-------------------------------------------------------------------------------------------------------------
 
-MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5, 
+MSE_func_FRQ = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5, 
                     Btarg="BMSY", Bconst=1, Ftarg="FMSY", Fconst=1, a=0.1, b=1, 
                     BuildPar=T, OMdirs=list(OMdir), 
                     simYrs=100, niters=NA, seed=430, sourcedir, SR="LFSR", implement="default", ...) {
@@ -73,7 +73,15 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
   #-------------------------------------------------------------------------------------------------------------
   
   # Use the source() file to read in functions from other r.scripts
-  source(file.path(sourcedir,"BuildParFile.R")) # BuildParFile
+  if(SR=="LFSR"){
+    source(file.path(sourcedir,"BuildParFile.R")) # BuildParFile
+  }
+  if(SR=="BH"){
+    source(file.path(sourcedir,"BuildParFile_BH.R")) # BuildParFile
+  }
+  if(SR=="lnR0"){
+    source(file.path(sourcedir,"BuildParFile_lnR0.R")) # BuildParFile
+  }
   
   source(file.path(sourcedir,"RunOM_NoHess.R")) # RunOM_NoHess
   source(file.path(sourcedir,"BuildEMDatFile.R")) # BuildEMDatFile
@@ -81,15 +89,25 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
   source(file.path(sourcedir,"UpdateOMDatFile.R")) # UpdateOMDatFile
   source(file.path(sourcedir,"RunEM.R")) # RunEM
   source(file.path(sourcedir,"HCR.R")) # HCR
+  source(file.path(sourcedir,"EditStarterFile.R")) # ImplementHCR
+  
   if(implement=="default"){
     source(file.path(sourcedir,"ImplementHCR.R")) # ImplementHCR
+    source(file.path(sourcedir,"UpdateCatchesAnnual.R")) # Annual
   }
   if(implement=="MexRec"){
     source(file.path(sourcedir,"ImplementHCR_MexRec.R")) # ImplementHCR
+    source(file.path(sourcedir,"UpdateCatchesAnnual_MexRec.R")) # ImplementHCR
   }
-  source(file.path(sourcedir,"UpdateCatchesAnnual.R")) # ImplementHCR
-  source(file.path(sourcedir,"EditStarterFile.R")) # ImplementHCR
+  if(implement=="LoMexRec"){
+    source(file.path(sourcedir,"ImplementHCR.R")) # ImplementHCR
+    source(file.path(sourcedir,"UpdateCatchesAnnual_LoMexRec.R")) # ImplementHCR
+  }
   # copy files to *save results* folder; maybe save relevant results in .Rdata form
+  
+  #convert data files to starter version. 
+  file.copy(from=file.path(OMdir,"SB_START.dat"),to=file.path(OMdir,"SB.dat"), overwrite=T)
+  file.copy(from=file.path(EMdir,"SB_START.dat"),to=file.path(EMdir,"SB.dat"), overwrite=T)
   
   # required libraries
   library(r4ss)
@@ -114,11 +132,11 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
   EndYear = OMdat$endyr
   StartSimYear = EndYear - (simYrs-1)
   
-  #get niters as sequence
-  if(length(niters)<=1){
-    ni = ifelse(is.na(niters), 1000, niters)
-    niters = 1:ni
-  }
+  # #get niters as sequence
+  # if(length(niters)<=1){
+  #   ni = ifelse(is.na(niters), 1000, niters)
+  #   niters = 1:ni
+  # }
   
   for(i in niters) {
     # i=1
@@ -127,7 +145,7 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
       #-------------------------------------------------------------------------------------------------------------
       # Build Par File
       #-------------------------------------------------------------------------------------------------------------
-      BuildParFile(MCMCdir,mcmc,i,OMdirs=list(OMdir) , SR=SR)
+      BuildParFile(MCMCdir,mcmc,i,OMdirs=list(OMdir) )
     } # End if BuildPar==T
 
     
@@ -152,7 +170,7 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
       } #end if tt==1
       
       if(tt>StartSimYear){              # this is to include future observation error
-        UpdateOM(OMdir, tt)             # NOTE: This is only to update annually. 
+        UpdateOM_annual(OMdir, t=tt)             # NOTE: This is only to update annually. 
         UpdateEM(EMdir, OMdir, FRQ, tt)
       } #end if tt>1
       
@@ -185,8 +203,10 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
           if(yt<FRQ){
             EditStarterFile(OMdir, seed+yt, tt)
             RunOM_NoHess(OMdir)
+            UpdateOM_annual(OMdir, t=tt+yt)             # NOTE: This is only to update annually. 
           } # end yt<FRQ
         } # END ANNUAL yt OM UPDATE   
+        
         
       } # End HCR and implementHCR
       
@@ -209,8 +229,8 @@ MSE_func = function(MCMCdir, OMdir, EMdir, StoreResults, FRQ=5,
     # save(MSEResults, file=paste0(StoreResults,"\\MSEResults.RData"))
     
     #convert data files to starter version. 
-    file.copy(from=file.path(OMdir,"SB - START.dat"),to=file.path(OMdir,"SB.dat"), overwrite=T)
-    file.copy(from=file.path(EMdir,"SB - START.dat"),to=file.path(EMdir,"SB.dat"), overwrite=T)
+    file.copy(from=file.path(OMdir,"SB_START.dat"),to=file.path(OMdir,"SB.dat"), overwrite=T)
+    file.copy(from=file.path(EMdir,"SB_START.dat"),to=file.path(EMdir,"SB.dat"), overwrite=T)
     
     
   }  # end for i:nrow(mcmc); iteration loop
